6c8e472a6fc3c8b10f738ed89df866d089c7f0dc
Hector Gabriel Aguero <gabrielhaguero@gmail.com>
2025-03-25 10:47:01 -0300

    add to mode dark

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import data from "../data.json";
 import { useState } from "react";
 
 function App() {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(true);
 
   const darkMode = () => {
     setIsDark(!isDark);
@@ -27,7 +27,7 @@ function App() {
             type="button"
             id="theme-toggle"
           >
-            {!isDark ? (
+            {isDark ? (
               <img
                 src="/public/images/icon-sun.svg"
                 alt="logo"
@@ -76,7 +76,7 @@ function App() {
           {data.map((item) => (
             <div
               key={item.id}
-              class="flex flex-col items-start justify-start gap-4 rounded-xl px-4 py-4 bg-gray-800 border-1 border-gray-400 dark:bg-gray-100 dark:text-white"
+              class="flex flex-col items-start justify-start gap-4 rounded-xl px-4 py-4 text-gray-300 bg-gray-800 border-1 border-gray-400 dark:bg-gray-100 dark:text-gray-800"
             >
               <div class="flex gap-4 justify-center items-center">
                 <img src={item.logo} alt={item.name} class="w-12 h-auto mb-4" />
@@ -88,14 +88,14 @@ function App() {
               <div class="flex gap-4 items-center justify-between mt-8 w-full">
                 <a
                   href="#"
-                  class="py-1 px-4 rounded-3xl bg-gray-700 border-1 border-gray-400 text-white"
+                  class="py-1 px-4 rounded-3xl bg-gray-700 border-1 border-gray-400 text-white dark:bg-transparent dark:text-gray-500"
                 >
                   Remove
                 </a>
 
                 <label for="name" class="text-white text-end">
                   <inpu type="checkbox" value="" class="sr-only peer" />
-                  <div class="relative w-9 h-5 bg-red-400 peer rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                  <div class="relative w-9 h-5 bg-red-400 peer rounded-full peer dark:bg-red-500 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
                 </label>
               </div>
             </div>
